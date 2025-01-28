614ce546cd9c9d0d45f0732fd2ee9518f399c486
AsahelMarq <amSalangsang@mcm.edu.ph>
2025-01-28 13:26:00 +0800

    Add files via upload

---
+++ PersonalDetails.java
@@ -0,0 +1,13 @@
+public class Part1 {
+    public static void main(String[] args) {
+        String name = "Asahel Marq T. Salangsang";
+        int age = 18;
+        double height = 1.61;
+        boolean isStudent = true;
+ 
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " meters");
+        System.out.println("Is a Student? " + isStudent);
+    }
+}
